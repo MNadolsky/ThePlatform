0ab8707c86b79851061e12c59636aef7fb679f16
Anthoy <AnthonyGrant200@hotmail.com>
2020-10-07 21:35:19 -0500

    hopefully final commit for home page object 10.7.20 3rd

--- pages/coinbase/homepage.py
+++ pages/coinbase/homepage.py
@@ -1,12 +1,10 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC 
-from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.common.by import By
 import secure.cookies
 import config
 
 
+sign_in_button_loc = (By.XPATH,"//a[@title='Sign in']")
 
 class Home_Page():
 
@@ -14,17 +12,7 @@ class Home_Page():
         self.driver = driver
         driver.get(config.coinbase_homepage_url)
 
-    #Buttons
 
-    # coinbase has two-factor authentication which involves sending a text
-    # via cellphone; it is optional to forego this authentication, so 
-    # cookies are added below to do this. It is currently unclear which cookie controls 
-    # the authentication. So, I just signed in, checked the option to not ask for
-    # the text code again, then signed out and grabbed all the cookies. Below I loaded 
-    # all these cookies which include the cookie/s necessary to forego this second 
-    # authentication.
-
-    def home_page_sign_in_button_loc(self): return (By.XPATH,"//a[@title='Sign in']")
     def click_home_page_sign_in_button(self):
-        self.driver.find_element(*self.home_page_sign_in_button_loc()).click()
-        for cookie in secure.cookies.coinbase: self.driver.add_cookie(cookie)
+        self.driver.find_element(*sign_in_button_loc).click()
+
