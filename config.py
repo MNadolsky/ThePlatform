a71a5fe6d9f1718e80a577dddde3128b552afb33
Anthoy <AnthonyGrant200@hotmail.com>
2020-11-11 14:16:38 -0600

    config fix 11.11.20

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 import os
 
-win_chromedriver_path = os.path.dirname(
-    os.path.realpath(__file__)) + '/resources/chromedriver.exe'
+win_chromedriver_path = '/resources/chromedriver.exe'
 mac_chromedriver_path = '/resources/chromedriver'
 
 coinbase_domain = 'https://www.coinbase.com'
