b5b2e82224ad4eb8076951471b79f03a787a2031
Anthony Grant <AnthonyGrant200@hotmail.com>
2020-11-25 16:01:01 -0600

    small update, delete file 11.25.20

--- coinbase_tests.py
+++ coinbase_tests.py
@@ -150,10 +150,37 @@ class Logout(unittest.TestCase):
 
         self.page.driver.quit()
 
+class HomePageSetup(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(root_path + config.win_chromedriver_path)
+        self.page = HomePage(self.driver)
+    
+    def tearDown(self):
+
+        self.page.driver.quit()
 
 
+class HomePageSignIn(HomePageSetup):
+    """
+    Ensure clicking on the sign in link will navigate the user to the sign in 
+    page.
 
+    acceptance criteria
+    ------------------- 
+    -Clicking the sign in button navigates to the sign in url.
+    """
 
+    def testSignIn(self):
+
+        page = self.page
+
+        page.sign_in_link.click()
+
+        self.assertEqual \
+            (page.driver.title, 'Coinbase - Buy/Sell Cryptocurrency')
+        self.assertEqual \
+            (page.driver.current_url,'https://www.coinbase.com/signin')
 
 
 if __name__ == '__main__': unittest.main()
